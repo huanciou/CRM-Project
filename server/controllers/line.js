3ea5180244141f6cbd8e032daa91c1fd29dd20ae
huanciou <huanciou@github.com>
2023-12-29 14:35:19 +0800

    Docs: Update LINE API Flex Message Uri

--- server/controllers/line.js
+++ server/controllers/line.js
@@ -115,8 +115,7 @@ function handleEvent(event) {
             height: 'sm',
             action: {
               type: 'uri',
-              label: 'CARD',
-              // uri: `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2001826171&redirect_uri=${LOCATION_ORIGIN}/user/signInCallback&state=123456789&scope=profile%20openid%20email&ui_locales=zh-TW&bot_prompt=normal`,
+              label: 'iSEECRM 會員系統',
               uri: 'https://iseecrm.fake-shop.store/user/signIn/test',
             },
           },
@@ -126,9 +125,8 @@ function handleEvent(event) {
             height: 'sm',
             action: {
               type: 'uri',
-              label: 'WEBSITE',
-              // uri: `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2001826171&redirect_uri=${LOCATION_ORIGIN}/user/signInCallback&state=123456789&scope=profile%20openid%20email&ui_locales=zh-TW&bot_prompt=normal`,
-              uri: 'https://iseecrm.fake-shop.store/user/signIn/test',
+              label: '測試登入',
+              uri: 'https://iseecrm.fake-shop.store/user/testLogin',
             },
           },
           {
